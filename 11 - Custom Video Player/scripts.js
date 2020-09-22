0c94561e4b8c6440c29723e21de2b74732bde030
Waldemar Adams <waldek-adamski@wp.pl>
2020-09-22 21:14:46 +0200

    fn:start/stop video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,15 @@
 const player = document.querySelector('.player');
-const video = player.querySelector('.viever');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toogle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function tooglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
